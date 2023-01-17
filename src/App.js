044ddfa3622e8857f0ec37e405ea1c31f8ba2d60
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2023-01-16 21:49:33 -0300

    resolve exercise

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            str: '',
+        }
+    }
+    handlerOnChange = (e) => {
+        const value = e.target.value
+        console.log(value)
+        this.setState({
+            str: value
+        });
+    };
+
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo"  onChange={ this.handlerOnChange}/>
+        <p className="repeater">{this.state.str}</p>
       </div>
     );
   }
